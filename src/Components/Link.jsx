795ae6c47d183c500a333c315167078d418e1d86
monurajputrko <monurko99@gmail.com>
2024-02-20 00:30:08 +0530

    updated

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -56,10 +56,20 @@ const { formData, setFormData } = useContext(AuthContext);
         <p className="payingtext">You are paying ₹ {amount}</p>
         <p className="payingtext">to</p>
         <p className="payingtext vpatext">{recipient}</p>
+        {sender !== "" && <p className="payingtext vpatext">({sender})</p> }
 
         <div className="center">
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            Go to External Website
+          <a
+            href={link}
+            className="inline-flex border-2 items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 btn"
+          >
+            <img
+              src="https://ik.imagekit.io/pngserver/upi-p-logo.png"
+              alt="UPI Icon"
+              className="w-4 h-4 mr-2"
+            />
+            {/* <!-- Replace the CDN link and alt text with your icon URL and description --> */}
+            Pay Now
           </a>
         </div>
       </section>
